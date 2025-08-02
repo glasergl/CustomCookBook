e0bb6a97a7fa5fb375e86611b9f6eaef3c92c161
glasergl <gabriel.glaser@web.de>
2025-08-02 03:09:17 +0200

    add group name field to recipe component

--- src/main/java/todo/custom/cook/book/ui/RecipeEditor.java
+++ src/main/java/todo/custom/cook/book/ui/RecipeEditor.java
@@ -13,6 +13,7 @@ import todo.custom.cook.book.entity.Recipe;
 public final class RecipeEditor {
     private final JPanel recipeEditorPanel = new JPanel();
     private final JTextField nameInput;
+    private final JTextField groupInput;
     private final JTextField durationInput;
     private final JTextField numberOfPortionsInput;
     private final JTextArea stepsInput;
@@ -20,6 +21,7 @@ public final class RecipeEditor {
 
     public RecipeEditor(final Recipe recipe) {
 	this.nameInput = new JTextField(recipe.name());
+	this.groupInput = new JTextField(recipe.group());
 	this.stepsInput = new JTextArea(getStepsAsString(recipe.steps()));
 	this.durationInput = new JTextField(recipe.duration());
 	this.numberOfPortionsInput = new JTextField(String.valueOf(recipe.numberOfPortions()));
@@ -29,6 +31,7 @@ public final class RecipeEditor {
 
     public RecipeEditor() {
 	this.nameInput = new JTextField();
+	this.groupInput = new JTextField();
 	this.stepsInput = new JTextArea();
 	this.durationInput = new JTextField();
 	this.numberOfPortionsInput = new JTextField();
@@ -39,12 +42,14 @@ public final class RecipeEditor {
     private void setupRecipePanel() {
 	recipeEditorPanel.setLayout(new BoxLayout(recipeEditorPanel, BoxLayout.Y_AXIS));
 	recipeEditorPanel.add(UICustomization.getLeftLabelledComponent(nameInput, "Name:", 5));
+	recipeEditorPanel.add(UICustomization.getLeftLabelledComponent(groupInput, "Gruppe:", 5));
 	recipeEditorPanel.add(UICustomization.getLeftLabelledComponent(numberOfPortionsInput, "Portionen:", 5));
 	recipeEditorPanel.add(UICustomization.getLeftLabelledComponent(durationInput, "Dauer:", 5));
 	recipeEditorPanel.add(ingredientsEditor.getPanel());
 	final JScrollPane scrollapeStepsInput = new JScrollPane(stepsInput, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 	recipeEditorPanel.add(UICustomization.getAboveLabelledComponent(scrollapeStepsInput, "Schritte:", 5));
 	nameInput.setColumns(20);
+	groupInput.setColumns(9);
 	durationInput.setColumns(9);
 	numberOfPortionsInput.setColumns(3);
 	stepsInput.setColumns(50);
